c0f97631d961ab78442d6928ccb21ca69079c3f2
Jeremiah Harriott <harriott_47@hotmail.com>
2023-11-18 16:15:32 -0500

    Basic script and template added

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref, onMounted, computed, watch } from 'vue'
+
+const name = ref('')
+const todos = ref([])
+const input_content = ref('')
+const input_category = ref(null)
+
+const input_basic = ref('')
+
+const todos_asc = computed(() => todos.value.sort((a, b) => {
+  return b.createdAt - a.createdAt
+}))
+</script>
+
+
+<template>
+<main class="app">
+  <section class="greeting">
+    <h2 class="title">
+      Hey, how are you? <input type="text" placeholder="Name here" v-model="name" />
+    </h2>
+    <section>
+      <h1 class="title">
+        Todos: {{todos.length}}
+      </h1>
+    </section>
+  </section>
+</main>
+</template>
